de0eedea4c3c1f73df4aed5586e1278f37c13a01
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2024-10-18 09:00:51 -0300

    Criação do método doGet() para a criação da página.

--- src/main/java/br/edu/ifsp/dsw1/HelloServlet.java
+++ src/main/java/br/edu/ifsp/dsw1/HelloServlet.java
@@ -6,20 +6,51 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private String messages[];
+	private int position;
        
     public HelloServlet() {
         super();
+        messages = new String[] {
+        		"Hello, World!",
+        		"Olá, Mundo!",
+        		"¡Hola, Mundo!",
+        		"Bonjour, le Monde!",
+        		"Hallo, Welt!"
+        };
+        position = -1;
     }
 
+    @Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		//try-with-resources, tem nos materiais de aula antigos
+		try(PrintWriter out = response.getWriter()){
+			out.println("<html>");
+			out.println("<head>");
+			out.println("<title>Olá Servlet!</title>");
+			out.println("</head>");
+			out.println("<body>");
+			out.println("<h1>"+ messages[usePosition()] +"</h1>");
+			out.println("</body>");
+			out.println("</html>");
+		}
 	}
 
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
 
+	private int usePosition() {
+		position++;
+		if(position==5) {
+			position=0;
+		}
+		return position;
+	}
 }
